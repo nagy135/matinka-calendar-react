a7a5e437dee199c041e3791ca6a009d52aac5a03
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-01-04 11:59:38 +0100

    feat: show attendant names

--- src/components/LessonDetail.js
+++ src/components/LessonDetail.js
@@ -11,24 +11,29 @@ const LessonDetail = (props) => {
 
     const reserveLesson = async () => {
         await axios.post(attendantsUrl,
-            {
-                firstName: attendant.firstName,
-                lastName: attendant.lastName,
-                email: attendant.email,
-                recordId: guestRecord.id
-            }
-        );
+                         {
+                             firstName: attendant.firstName,
+                             lastName: attendant.lastName,
+                             email: attendant.email,
+                             recordId: guestRecord.id
+                         }
+                        );
         alert("Hodina rezervovana uspesne");
         props.closeGuestModal();
     }
 
     const getAttendantCount = async (recordId) => {
         const response = await axios.get(attendantsUrl + "/" + recordId + " /count");
-        console.log('now');
         setAttendantCount(response.data.data.attendantCount);
     }
 
+    const getAttendantNames = async (recordId) => {
+        const response = await axios.get(attendantsUrl + "/" + recordId + " /names");
+        setAttendantNames(response.data.data.attendants);
+    }
+
     const [attendantCount, setAttendantCount] = useState(0);
+    const [attendantNames, setAttendantNames] = useState([]);
 
     const [attendant, setAttendant] = useState({
         firstName: '',
@@ -46,80 +51,85 @@ const LessonDetail = (props) => {
         else setCanSubmit(false);
     }
     useEffect(() => {
-        getAttendantCount(guestRecord.id)
+        getAttendantCount(guestRecord.id);
+        getAttendantNames(guestRecord.id);
     }, []);
 
     if (guestRecord){
         return (
             <div>
-                <Container className="record">
-                    <Row>
-                        <Col lg="3">#</Col>
-                        <Col>{guestRecord.id}</Col>
-                    </Row>
-                    <Row>
-                        <Col lg="3">Názov</Col>
-                        <Col>{guestRecord.title}</Col>
-                    </Row>
-                    <Row>
-                        <Col lg="3">Popis</Col>
-                        <Col>{guestRecord.description}</Col>
-                    </Row>
-                    <Row>
-                        <Col lg="3">prihlásených</Col>
-                        <Col>{attendantCount}</Col>
-                    </Row>
-                </Container>
-                <hr />
-                <Form
-                    className="attend-form"
-                    onSubmit={reserveLesson}
+              <Container className="record">
+                <Row>
+                  <Col lg="3">#</Col>
+                  <Col>{guestRecord.id}</Col>
+                </Row>
+                <Row>
+                  <Col lg="3">Názov</Col>
+                  <Col>{guestRecord.title}</Col>
+                </Row>
+                <Row>
+                  <Col lg="3">Popis</Col>
+                  <Col>{guestRecord.description}</Col>
+                </Row>
+                <Row>
+                  <Col lg="3">prihlásených ({attendantCount})</Col>
+                  <Col className="attendants-names">
+                    <ul>
+                      {attendantNames.map((e,i) => <li key={i}>{e}</li>)}
+                    </ul>
+                  </Col>
+                </Row>
+              </Container>
+              <hr />
+              <Form
+                className="attend-form"
+                onSubmit={reserveLesson}
+              >
+                <Form.Group controlId="firstName">
+                  <Form.Label>Meno</Form.Label>
+                  <Form.Control
+                    onChange={inputChange}
+                    name="firstName"
+                    value={attendant.firstName}
+                    type="text"
+                    placeholder="Tvoje meno"
+                  />
+                </Form.Group>
+                <Form.Group controlId="lastName">
+                  <Form.Label>Priezvisko</Form.Label>
+                  <Form.Control
+                    onChange={inputChange}
+                    name="lastName"
+                    value={attendant.lastName}
+                    type="text"
+                    placeholder="Tvoje priezvisko"
+                  />
+                </Form.Group>
+                <Form.Group controlId="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    onChange={inputChange}
+                    name="email"
+                    value={attendant.email}
+                    type="text"
+                    placeholder="Tvoj email"
+                  />
+                </Form.Group>
+                <Button
+                  className="reserve-lesson"
+                  variant="success"
+                  disabled={!canSubmit}
+                  type="submit"
                 >
-                    <Form.Group controlId="firstName">
-                        <Form.Label>Meno</Form.Label>
-                        <Form.Control
-                            onChange={inputChange}
-                            name="firstName"
-                            value={attendant.firstName}
-                            type="text"
-                            placeholder="Tvoje meno"
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="lastName">
-                        <Form.Label>Priezvisko</Form.Label>
-                        <Form.Control
-                            onChange={inputChange}
-                            name="lastName"
-                            value={attendant.lastName}
-                            type="text"
-                            placeholder="Tvoje priezvisko"
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                            onChange={inputChange}
-                            name="email"
-                            value={attendant.email}
-                            type="text"
-                            placeholder="Tvoj email"
-                        />
-                    </Form.Group>
-                    <Button
-                        className="reserve-lesson"
-                        variant="success"
-                        disabled={!canSubmit}
-                        type="submit"
-                    >
-                        Prihlasujem sa
-                    </Button>
-                </Form>
+                  Prihlasujem sa
+                </Button>
+              </Form>
             </div>
         )
     } else {
         return (
             <div>
-                loading...
+              loading...
             </div>
         )
     }
